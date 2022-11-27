84e3a3656814db97655e8cf76408919a1f56a959
Pablo <paba.0018@gmail.com>
2022-11-27 13:08:30 +0100

    footer content

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,16 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import { ReactComponent as FacebookSvg } from "../../assets/facebook.svg";
+import { ReactComponent as InstagramSvg } from "../../assets/instagram.svg";
+
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.footer}>
       <h2>Obserwuj nas</h2>
+      <div className={styles.svgContainer}>
+        <FacebookSvg className={styles.svg} />
+        <InstagramSvg className={[styles.svg, styles.svgIg].join(" ")} />
+      </div>
     </div>
   );
 };
